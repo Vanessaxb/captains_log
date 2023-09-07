9462dca569dc10010822859d7cf41be4186dedb3
Vanessa Beattie <your_git_email@email.com>
2023-09-07 13:27:59 -0500

    Create Route done

--- server.js
+++ server.js
@@ -1,11 +1,44 @@
-const express = require('require')
+const express = require('express');
+// require ('dotenv').config();
+const jsxEngine = require("jsx-view-engine")
 
-const app = express
+//^ ======= Variables
+const app = express()
 const PORT = 3000
 
+//^ ======== App Config
+app.set("view engine", "jsx");
+app.engine("jsx", jsxEngine());
 
+//^ ======= Middleware
+app.use(express.urlencoded({ extended: false}))
+app.use(express.json()) 
+
+//^ ==== Routes
+/**
+ * Root
+ */
+app.get("/", (req, res) => {
+    res.send('Working')
+})
+
+/**
+ * ^ New
+ */
 app.get("/logs/new", (req, res) => {
+    if (req.body.shipIsBroken === "on") {
+        req.body.shipIsBroken = true;
+    } else {
+        req.body.shipIsBroken = false;
+    }
     res.render('New')
+})
+
+/**
+ * ^ Create
+ */
+app.post("/logs", (req, res) => {
+    res.send(req.body)
 })
 
 app.listen(PORT, () => {
